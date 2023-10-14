70117689672906fc6149e8e03d0859ee7acf1395
MiixZ <xwarzmiixz@gmail.com>
2023-10-14 21:10:27 +0200

    primera versión práctica 1

---
+++ project/P1-DBA/src/p1DBA/ejercicio04.java
@@ -0,0 +1,93 @@
+package p1DBA;
+
+import jade.core.Agent;
+import jade.core.behaviours.WakerBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author isma
+ */
+public class ejercicio04 extends Agent {
+    public final int TIEMPO_ESPERA = 2000;
+    Scanner escaner = new Scanner(System.in);
+    ArrayList<Float> numeros = new ArrayList<>();
+    Boolean media_calculable = false; 
+    int n_numeros;
+    
+    WakerBehaviour pedirN_numeros = new WakerBehaviour(this, TIEMPO_ESPERA) {
+        @Override
+        public void onWake() {
+            System.out.println("\nMODO 0: Introduzca un número positivo" +
+                     " de elementos numéricos a leer:\n");
+            
+            n_numeros = escaner.nextInt();
+        }
+    };
+    
+    TickerBehaviour pedirNumeros = new TickerBehaviour(this, TIEMPO_ESPERA) {
+        int i = 0;
+
+        @Override
+        public void onTick() {
+            float n_actual;
+
+            if (n_numeros > 0) {
+                try {
+                    System.out.println("\nMODO 1: Cogiendo número...\n");
+                    n_actual = escaner.nextFloat();
+                    numeros.add(n_actual);
+                    i++;
+                } catch (Exception e) {
+                    System.out.println("\nMODO 1: No ha introducido un número válido"
+                            + " en la zona " + i + "... Introduzca un sustituto...\n");
+                    
+                    escaner.next();
+                }
+            }
+
+            if (i >= n_numeros) {
+                media_calculable = true;
+                stop();
+            }
+        }
+    };
+
+    TickerBehaviour calcularMedia = new TickerBehaviour(this, TIEMPO_ESPERA*4) {
+        @Override
+        public void onTick() {
+            if (media_calculable) {
+                float suma = 0;
+
+                for (int i = 0; i < numeros.size(); i++) {
+                    suma += numeros.get(i);
+                }
+
+                System.out.println("\nMODO 2: La media es: " + suma/n_numeros);
+                stop();
+                doDelete();
+            } else {
+                System.out.println("\nMODO 2: Esperando a recibir todos los números...\n");
+            }
+        }
+    };
+    
+    @Override
+    protected void setup() {
+        // Este comportamiento pedirá el número de elementos a leer.
+        this.addBehaviour(pedirN_numeros);
+
+        // Este será un comportamiento cíclico que pida los datos hasta que sean correctos.
+        this.addBehaviour(pedirNumeros);
+
+        // Este comportamiento calculará la media.
+        this.addBehaviour(calcularMedia);   
+    }
+    
+    @Override
+    public void takeDown() {
+        System.out.println("Terminando agente...");
+    }
+}
